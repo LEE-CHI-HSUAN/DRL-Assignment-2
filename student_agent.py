6fbd969cb01e043033079605f2bb4c2bc384f732
jameslee <b10902111@ntu.edu.tw>
2025-04-11 18:15:45 +0800

    use msc

--- student_agent.py
+++ student_agent.py
@@ -7,11 +7,29 @@ from gym import spaces
 import matplotlib.pyplot as plt
 import copy
 import random
-from game2048 import Game2048Env
+from game2048 import Game2048Env, NTupleApproximator
+from monte_carlo import MCTS_Node, MCS
+
+
+patterns = [
+    ((0, 0), (0, 1), (1, 0), (1, 1)),  # corner square
+    ((0, 1), (0, 2), (1, 1), (1, 2)),  # edge square
+    ((0, 0), (0, 1), (0, 2), (0, 3)),  # edge line
+    ((1, 0), (1, 1), (1, 2), (1, 3)),  # middle line
+]
+approximator = NTupleApproximator(board_size=4, patterns=patterns, gamma=0.99)
+with open("value_weights.pkl", "rb") as fin:
+    approximator.LUTs = pickle.load(fin)
+
+env = Game2048Env()
+mcs = MCS(env, approximator, batch_size=5)
 
 
 def get_action(state, score):
-    env = Game2048Env()
-    return random.choice([0, 1, 2, 3])  # Choose a random action
+    env.board = state
+    env.score = score
+    root = MCTS_Node(None, None, env)
+    action = mcs.simulate(root, steps=5)
+    return action
 
     # You can submit this random agent to evaluate the performance of a purely random strategy.
